890be1fffb86cb59da6d8be7a5776190fd86fec4
Evan <evanahouse@gmail.com>
2021-12-09 21:43:23 -0800

    break points mobile

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ const highlightMenu = () => {
     audioMenu.classList.remove("highlight");
     // videoMenu.classList.remove("highlight");
     // contactMenu.classList.remove("highlight");
-  } else if (window.innerWidth > 900 && scrollPos < 3000 && scrollPos > 900) {
+  } else if (window.innerWidth > 900 && scrollPos < 2900 && scrollPos > 900) {
     audioMenu.classList.add("highlight");
     homeMenu.classList.remove("highlight");
     videoMenu.classList.remove("highlight");
     // contactMenu.remove("highlight");
-  } else if (window.innerWidth > 900 && scrollPos > 2945 && scrollPos < 4300) {
+  } else if (window.innerWidth > 900 && scrollPos > 2900 && scrollPos < 3900) {
     videoMenu.classList.add("highlight");
     audioMenu.classList.remove("highlight");
     contactMenu.classList.remove("highlight");
     // homeMenu.classList.remove("highlight");
-  } else if (window.innerWidth > 900 && scrollPos > 4300) {
+  } else if (window.innerWidth > 900 && scrollPos > 3900) {
     contactMenu.classList.add("highlight");
     // audioMenu.classList.remove("highlight");
     videoMenu.classList.remove("highlight");
     // homeMenu.classList.remove("highlight");
   }
-  if ((elem && window.innerWidth < 900)) {
+  if (elem && window.innerWidth < 900) {
     elem.classList.remove("highlight");
   }
 };
@@ -42,6 +42,44 @@ const audioLink = document.querySelector("#audio-page");
 const videoLink = document.querySelector("#video-page");
 const contactLink = document.querySelector("#contact-page");
 
+let audioPoint, videoPoint, contactPoimport;
+
+if (window.innerWidth > 900) {
+  audioPoint = 950;
+  videoPoint = 2950;
+  contactPoint = 4300;
+}
+
+if (window.innerWidth < 900 && window.innerWidth > 811) {
+  audioPoint = 900;
+  videoPoint = 2550;
+  contactPoint = 3900;
+}
+
+if (window.innerWidth < 811 && window.innerWidth > 753) {
+  audioPoint = 850;
+  videoPoint = 2050;
+  contactPoint = 3375;
+}
+
+if (window.innerWidth < 753 && window.innerWidth > 603) {
+  audioPoint = 850;
+  videoPoint = 2350;
+  contactPoint = 3725;
+}
+
+if (window.innerWidth < 603 && window.innerWidth > 500) {
+  audioPoint = 700;
+  videoPoint = 3500;
+  contactPoint = 4625;
+}
+
+if (window.innerWidth < 500) {
+  audioPoint = 700;
+  videoPoint = 3550;
+  contactPoint = 4675;
+}
+
 const scrollHome = () => {
   window.scroll({
     top: 0,
@@ -51,21 +89,21 @@ const scrollHome = () => {
 };
 const scrollAudio = () => {
   window.scroll({
-    top: 1050,
+    top: audioPoint,
     left: 0,
     behavior: "smooth",
   });
 };
 const scrollVideo = () => {
   window.scroll({
-    top: 3050,
+    top: videoPoint,
     left: 0,
     behavior: "smooth",
   });
 };
 const scrollContact = () => {
   window.scroll({
-    top: 4600,
+    top: contactPoint,
     left: 0,
     behavior: "smooth",
   });
